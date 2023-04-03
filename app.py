d2835e9ecc0804fd75c14d5d48e6e40aab4a53c3
Pete Curran <Curranpete@gmail.com>
2023-04-03 20:31:01 +0100

    Cleaning up for first release

--- app.py
+++ app.py
@@ -4,12 +4,14 @@ from print_generator import crosswordPrinter, generateImage
 import config
 import prompts
 import openai
-import time # For testing
 
 # Get the openai key from config.py
+# This is a file that is not included in the repository. It only requires one line:
+# openAIKey = 'YOUROPENAIKEY'
 openai.api_key = config.openAIKey
 
-numClues = 20
+numAttempts = 20 # Number of attempts to generate a crossword. Higher is better but slower. Go and grab a coffee.
+numClues = 20 # Number of clues to include. 20-40 works well. More theoretically possible but the crossword will be very small.
 theme = input("What is the theme of the crossword?\n")
 
 # Test the openai call
@@ -21,19 +23,14 @@ response = openai.ChatCompletion.create(
         {"role":"user","content":"The topic for this crossword is '{}'.".format(theme)}
     ])
 
-content = response.choices[0].message.content
-clues = parseClues(content)
-print(clues)
+content = response.choices[0].message.content # Get the relevant bit from GPT's structured response
+clues = parseClues(content) # Get the clues from the response.
 
-# Start a timer
-start = time.time()
-generator = CrosswordGenerator(clues, 20)
-crossword = generator.generateCrossword()
-end = time.time()
-# Print the time taken in minutes and seconds
-print("Time taken: {} minutes and {} seconds".format(int((end-start)/60), int((end-start)%60)))
-crossword.printGrid()
-crossword.printBlankGrid()
+generator = CrosswordGenerator(clues, 20) # Create a crossword generator with the clues and the number of attempts
+crossword = generator.generateCrossword() # Generate the crossword
 print("Words added: {}".format(crossword.numWords))
 print("Intersections: {}".format(crossword.numberOfIntersections))
-crosswordPrinter(crossword, theme)
+
+crosswordPrinter(crossword, theme) # Send the crossword to HTML files for printing.
+print("Crossword saved to infiniteCrosswords/created_crosswords/")
+input("Press enter to exit.")
